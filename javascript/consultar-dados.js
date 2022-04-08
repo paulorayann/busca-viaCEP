99ca759debc5c360b969716eafa583343fed5762
Paulo Rayann <paulorpsena@gmail.com>
2022-04-08 17:43:06 -0300

    segundo commit

--- javascript/consultar-dados.js
+++ javascript/consultar-dados.js
@@ -3,31 +3,28 @@ let rua = document.querySelector('#rua');
 let bairro = document.querySelector('#bairro');
 let cidade = document.querySelector('#cidade');
 let estado = document.querySelector('#estado');
+let btnCep = document.querySelector('#btnBuscarCep');
 
-cep.value = '';
 
 cep.addEventListener('blur',function(e) {
-    let cep = e.target.value;
-    let script = document.createElement('script');
-    script.src = 'https://viacep.com.br/ws/' +cep+ '/json/?callback=consultaCEP';
-    document.body.appendChild(script);
-    
-
+        let cep = e.target.value;
+        let script = document.createElement('script');
+        script.src= 'https://viacep.com.br/ws/' +cep+ '/json/?callback=consultaCEP'
+        document.body.appendChild(script);
 });
 
-function consultaCEP(retorno) {
-    if("erro" in retorno) {
+function consultaCEP(conteudo) {
+    if("erro" in conteudo) {
         alert('CEP não encontrado');
         return;
     }
 
 
 
-
-    console.log(retorno);
-    rua.value = retorno.logradouro;
-    bairro.value = retorno.bairro;
-    cidade.value = retorno.localidade;
-    estado.value = retorno.uf;
+    console.log(conteudo);
+    rua.value = conteudo.logradouro;
+    bairro.value = conteudo.bairro;
+    cidade.value = conteudo.localidade;
+    estado.value = conteudo.uf;
 
 }
