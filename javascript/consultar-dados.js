1ced0be241df20dd81ab494a41adead80547bf07
Paulo Rayann <paulorpsena@gmail.com>
2022-04-10 01:01:29 -0300

    Alinhamento do botão e mudança da imagem no form

--- javascript/consultar-dados.js
+++ javascript/consultar-dados.js
@@ -1,41 +1,36 @@
-
-const logradouro = document.querySelector('#rua');
-const bairro = document.querySelector('#bairro');
-const cidade = document.querySelector('#cidade');
-const estado = document.querySelector('#estado');
-const hidden_input = document.querySelector('#hidden-input');
+const logradouro = document.querySelector("#rua");
+const bairro = document.querySelector("#bairro");
+const cidade = document.querySelector("#cidade");
+const estado = document.querySelector("#estado");
+const hidden_input = document.querySelector("#hidden-input");
 
 function consultaCEP() {
-
-    cep = document.getElementById("cep").value
-    apiURL = 'https://viacep.com.br/ws/' + cep + '/json'
-    pedido = new XMLHttpRequest()
-    pedido.open('GET', apiURL)
-    pedido.onerror = function(e) {
-        document.getElementById('resposta').innerHTML = 'CEP INVÁLIDO'
-        document.getElementById("cep").style.border = '1px solid red'
-    
+  cep = document.getElementById("cep").value;
+  apiURL = "https://viacep.com.br/ws/" + cep + "/json";
+  pedido = new XMLHttpRequest();
+  pedido.open("GET", apiURL);
+  pedido.onerror = function (e) {
+    document.getElementById("resposta").innerHTML = "";
+    document.getElementById("cep").style.border = "1.8px solid red";
+  };
+  pedido.onload = () => {
+    var conteudo = JSON.parse(pedido.responseText);
+    if (conteudo.erro === true) {
+      document.getElementById("resposta").innerHTML = "CEP NÃO ENCONTRADO";
+    } else {
+      hidden_input.style.display = "block";
+      console.log(conteudo);
+      logradouro.value = conteudo.logradouro;
+      bairro.value = conteudo.bairro;
+      cidade.value = conteudo.localidade;
+      estado.value = conteudo.uf;
     }
-    pedido.onload = () => {
-        var conteudo = JSON.parse(pedido.responseText)
-        if (conteudo.erro === true) {
-            document.getElementById('resposta').innerHTML = 'CEP NÃO ENCONTRADO'
-        } else {
-            hidden_input.style.display = 'block'
-            console.log(conteudo);
-            logradouro.value = conteudo.logradouro;
-            bairro.value = conteudo.bairro;
-            cidade.value = conteudo.localidade;
-            estado.value = conteudo.uf;
-        }
-    }
-    pedido.send();
-    logradouro.value = ''; 
-    bairro.value = ''; 
-    cidade.value = ''; 
-    estado.value = ''; 
-    hidden_input.style.display = 'none'
-    resposta.innerHTML = ''
-
-
+  };
+  pedido.send();
+  logradouro.value = "";
+  bairro.value = "";
+  cidade.value = "";
+  estado.value = "";
+  hidden_input.style.display = "none";
+  resposta.innerHTML = "";
 }
