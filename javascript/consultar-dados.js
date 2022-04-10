f6817b3b0b4c85a60d4026c012b82aeb6da302a7
Paulo Rayann <paulorpsena@gmail.com>
2022-04-10 15:12:20 -0300

    Mudança de posicionamento do botão, mudança da img e na mensagem de erro

--- javascript/consultar-dados.js
+++ javascript/consultar-dados.js
@@ -3,21 +3,46 @@ const bairro = document.querySelector("#bairro");
 const cidade = document.querySelector("#cidade");
 const estado = document.querySelector("#estado");
 const hidden_input = document.querySelector("#hidden-input");
+const loader = document.querySelector('.preload');
+const emoji = loader.querySelector('.emoji');
+
+
+const emojis = ["🕐", "🕜", "🕑","🕝", "🕒", "🕞", "🕓", "🕟", "🕔", "🕠", "🕕", "🕡", "🕖", "🕢",  "🕗", "🕣", "🕘", "🕤", "🕙",  "🕥", "🕚", "🕦",  "🕛", "🕧"];
+
+const interval = 125;
+
+const loadEmojis = (arr) => {
+    setInterval(() => {
+      emoji.innerText = arr[Math.floor(Math.random() * arr.length)];
+      console.log(Math.floor(Math.random() * arr.length))
+    }, interval);
+}
+
+
 
 function consultaCEP() {
   cep = document.getElementById("cep").value;
   apiURL = "https://viacep.com.br/ws/" + cep + "/json";
   pedido = new XMLHttpRequest();
-  pedido.open("GET", apiURL);
+  pedido.open("get", apiURL, true);
   pedido.onerror = function (e) {
-    document.getElementById("resposta").innerHTML = "";
-    document.getElementById("cep").style.border = "1.8px solid red";
+    document.getElementById("resposta").innerHTML = "INVÁLIDO!";
+    document.getElementById("cep").style.border = "2px solid #9400D3";
   };
+
+
   pedido.onload = () => {
     var conteudo = JSON.parse(pedido.responseText);
     if (conteudo.erro === true) {
       document.getElementById("resposta").innerHTML = "CEP NÃO ENCONTRADO";
+      
     } else {
+      const init = () => {
+        loadEmojis(emojis);
+      }
+      init();
+      document.querySelector(".preload").style.display = "none"//stop the load
+
       hidden_input.style.display = "block";
       console.log(conteudo);
       logradouro.value = conteudo.logradouro;
@@ -34,3 +59,11 @@ function consultaCEP() {
   hidden_input.style.display = "none";
   resposta.innerHTML = "";
 }
+
+
+
+const init = () => {
+  loadEmojis(emojis);
+}
+init();
+//document.querySelector(".preload").style.display = "none"//stop the load
